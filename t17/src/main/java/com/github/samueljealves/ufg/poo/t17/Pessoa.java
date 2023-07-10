ab6de228018735ccbe8497f4364fb448b7fbdd7f
SamuelJEAlves <samuelalvesevangelista@gmail.com>
2023-07-10 10:12:23 -0300

    Modelagem festa t17

--- t17/src/main/java/com/github/samueljealves/ufg/poo/t17/Pessoa.java
+++ t17/src/main/java/com/github/samueljealves/ufg/poo/t17/Pessoa.java
@@ -1,4 +1,21 @@
 package com.github.samueljealves.ufg.poo.t17;
 
 public class Pessoa {
+
+    private String nome;
+    private String sexo;
+
+    public Pessoa(String nome, String sexo) {
+        this.nome = nome;
+        this.sexo = sexo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
 }
+
